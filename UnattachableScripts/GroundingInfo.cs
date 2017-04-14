682bc8090183e1045d8831efc0a1bca19ccf294e
qonfi <confidentialrendezvous@gmail.com>
2017-04-14 20:59:16 +0900

    Add files via upload

---
+++ UnattachableScripts/GroundingInfo.cs
@@ -0,0 +1,19 @@
+﻿
+
+using UnityEngine;
+
+
+namespace Fiziks2D
+{
+
+
+    public class GroundingInfo
+    {
+        public GameObject LastDetectedGround { get; set; }
+        public GameObject GroundBeingDetected { get;   set; }
+        public bool IsGrounding { get;  set; }
+        public bool HasTakenOff { get;  set; } // ?
+    }
+
+
+}
