819281244dd780bf624a63d117f98a671546f551
qonfi <confidentialrendezvous@gmail.com>
2017-04-15 15:00:10 +0900

    Add files via upload

---
+++ Components/BounceSimulator2D.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+using UnityEngine;
+
+namespace Fiziks2D
+{
+    /// <summary>
+    /// コンポーネント。接地判定のインターフェイスに依存している。
+    /// </summary>
+    public class BounceSimulator2D : MonoBehaviour, IMovementCalculator2D
+    {
+        public Vector2 MovementPerFrame { get; set; }
+        private IGroundDetector2D detector;
+
+
+        private void Start()
+        {
+            detector = GetComponent<IGroundDetector2D>();
+        }
+
+
+        private void Update()
+        {
+            if (detector.Info == null) { return; }
+
+            if (detector.Info.IsGrounding)
+            {
+                MovementPerFrame = Vector2.up * 10f;
+            }
+
+        }
+    }
+}
---
+++ Components/FallerGroundDetector2D.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+
+namespace Fiziks2D
+{
+
+
+    public class FallerGroundDetector2D : MonoBehaviour, IGroundDetector2D
+    {
+        public IRayCaster2D Caster { get; set; }
+        public GroundingInfo Info { get; set; }
+
+
+        public void Start()
+        {
+            Caster = new OverlapCircleCaster2D();
+        }
+
+
+        public void Update()
+        {
+            Info = Caster.Cast(this.transform.position);
+        }
+
+
+    }
+
+
+}
