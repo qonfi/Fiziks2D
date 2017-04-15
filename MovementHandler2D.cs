b6a7fa31d80b29715b4311fd6a084d96cd811387
qonfi <confidentialrendezvous@gmail.com>
2017-04-15 17:29:36 +0900

    Add files via upload

---
+++ MovementHandler2D.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Fiziks2D
+{
+
+    /// <summary>
+    /// フレームごとの各コンポーネントの移動量を合計して実行するクラス。
+    /// </summary>
+    public class MovementHandler2D : MonoBehaviour
+    {
+        // CharacterController を2D でも使えれば使う予定だが、そのあたりが決まっていないのでいろいろ暫定。
+        // しかしかなり洗練されてきているので3Dのほうでもこのクラスは参考にしたい。
+
+        private List<IMovementCalculator2D> calculators;
+        public Vector2 TotalMovement { get; private set; }
+
+        private void Start()
+        {
+            calculators = new List<IMovementCalculator2D>();
+            calculators.AddRange(GetComponents<IMovementCalculator2D>());
+        }
+
+
+        private void Update()
+        {
+            TotalMovement = Vector2.zero;
+
+            foreach (IMovementCalculator2D calcator in calculators)
+            {
+                TotalMovement += calcator.MovementPerFrame;
+            }
+
+            transform.Translate(TotalMovement * Time.deltaTime);
+        }
+
+    }
+
+
+}
---
+++ PlatformerWalk2D.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using UnityEngine;
+
+
+namespace Fiziks2D
+{
+
+
+    public class PlatformerWalk2D : MonoBehaviour, IMovementCalculator2D
+    {
+        public Vector2 MovementPerFrame { get; set; }
+        public float WalkSpeed { get; set; }
+        public float MaxSpeed { get; set; }
+
+
+        private void Start()
+        {
+            // テスト用
+            WalkSpeed = 0.6f;
+            MaxSpeed = 3.6f;
+        }
+
+
+        private void Update()
+        {
+
+            if (Input.GetMouseButton(0))
+            {
+                MovementPerFrame += Vector2.right * -WalkSpeed;
+            }
+
+
+            if (Input.GetMouseButton(1))
+            {
+                MovementPerFrame += Vector2.right * WalkSpeed;
+            }
+
+
+            // 値を特定の範囲に収める。Mathf.Clamp のほうがいいか...?
+            float f = Mathf.Min(MaxSpeed, Math.Max(-MaxSpeed, MovementPerFrame.x));
+            MovementPerFrame = Vector2.right * f;
+
+
+            if (Input.GetMouseButton(0) == false &&
+                Input.GetMouseButton(1) == false)
+            {
+                MovementPerFrame *= 0.9f;
+            }
+
+        }
+    }
+
+
+}
