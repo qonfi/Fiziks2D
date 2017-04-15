b0b2805f105a1ea224644e6fafb35f1c2b291f8c
qonfi <confidentialrendezvous@gmail.com>
2017-04-15 15:01:20 +0900

    Add files via upload

---
+++ Interfaces/IGroundDetector2D.cs
@@ -0,0 +1,17 @@
+﻿
+
+using UnityEngine;
+
+
+namespace Fiziks2D
+{
+
+
+    public interface IGroundDetector2D
+    {
+       GroundingInfo Info { get; set; }
+       IRayCaster2D Caster { get; set; }
+    }
+
+
+}
---
+++ Interfaces/IRayCaster2D.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+
+namespace Fiziks2D
+{
+
+
+    public interface IRayCaster2D
+    {
+        GroundingInfo Cast(Vector2 origin);
+    }
+
+
+}
